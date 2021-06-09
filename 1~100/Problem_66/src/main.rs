5e1face03cd207425eed5b21bbaab4c56ddd0ffa
jameskk <jamesmarva@users.noreply.github.com>
2021-06-10 03:41:24 +0800

    add problem 66

---
+++ 1~100/Problem_66/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+}
+
+pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+    if digits.len() == 0 {
+        return digits;
+    }
+    let (mut i, mut carry_one) = (digits.len() as i32 - 1 , false);
+    let mut rst: Vec<i32> = digits.clone();
+    while i >= 0 {
+        let mut tmp = digits[i as usize] + 1;
+        if carry_one {
+            tmp += 1;
+        }
+        if tmp >= 10 {
+            rst[i as usize] = tmp - 10;
+            carry_one = true;
+        } else {
+            rst[i as usize] = tmp;
+            carry_one = false;
+        }
+        i -= 1;
+    }
+    if carry_one {
+        rst.insert(0, 1);
+    }
+    rst
+}
