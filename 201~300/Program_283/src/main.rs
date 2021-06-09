92e24e5a53170ea373de20949939378ac357887f
jameskk <jamesmarva@users.noreply.github.com>
2021-06-10 03:03:39 +0800

    add problem 283

---
+++ 201~300/Program_283/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    
+}
+
+pub fn move_zeroes(nums: &mut Vec<i32>) {
+    let (mut slow, mut fast, len) = (0, 0, nums.len());
+    while fast < len {
+        if nums[fast] == 0 {
+            fast += 1;
+        }
+
+        if nums[slow] != 0 {
+            if slow == fast {
+                fast += 1;
+            } 
+            slow += 1;
+        }
+
+        if fast < len && nums[slow] == 0 && nums[fast] != 0 {
+            nums.swap(slow, fast);
+        }
+    }
+}
