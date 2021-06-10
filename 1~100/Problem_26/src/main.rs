ae7700a196d6c6d03514c0d21359063dd4b68d67
jameskk <jamesmarva@users.noreply.github.com>
2021-06-11 04:03:14 +0800

    add 26

--- 1~100/Problem_26/src/main.rs
+++ 1~100/Problem_26/src/main.rs
@@ -4,13 +4,14 @@ pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
     if nums.len() == 0 {
         return 0;
     }
-    if nums.len() == 1 {
-        return 1;
-    }
-    let (mut slow, mut fast, mut pre) = (1usize, 1usize, nums[0]);
+    let (mut slow, mut fast) = (0usize, 0usize);
     while fast < nums.len() {
-        if pre == nums[fast] {
+        if nums[fast] == nums[slow] {
             fast += 1;
+        }
+        if fast < nums.len() && nums[fast] != nums[slow] {
+            slow += 1;
+            nums[slow] = nums[fast];
         }
     }
     slow as i32 + 1
