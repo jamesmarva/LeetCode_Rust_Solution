939a92f831cf9bf45d1f7bc0e3be2ca9f78030a6
jameskk <jamesmarva@users.noreply.github.com>
2021-06-16 00:16:04 +0800

    add 239 better code

--- 1~100/Problem_01/src/main.rs
+++ 1~100/Problem_01/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+
+}
+use std::collections::HashMap;
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut rst = Vec::with_capacity(2);
+    let mut map: HashMap<u32, i32> = HashMap::new();
+    for i in 0..nums.len() {
+        if map.contains_key()
+    }
+
 }
--- 201~300/Program_239/src/better.rs
+++ 201~300/Program_239/src/better.rs
@@ -1,5 +1,26 @@
 use std::collections::VecDeque;
 
 pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    
+    let mut rst = Vec::new();
+    let mut win = VecDeque::new();
+    let (mut max, k) = (i32::MAX, k as usize);
+    for i in 0..nums.len() {
+        if i >= k {
+            rst.push(max);
+            if nums[i - k] == *win.front().unwrap(){
+                win.pop_front();
+            } 
+        }
+        while let Some(&x) = win.back() {
+            if nums[i] > x {
+                win.pop_back();
+            } else {
+                break;
+            }
+        }
+        win.push_back(nums[i]);
+        max = *win.front().unwrap();
+    }
+    rst.push(max);
+    rst
 }
