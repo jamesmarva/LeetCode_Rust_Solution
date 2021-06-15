4227323b983531c577136dec487ee22944e3618c
jameskk <jamesmarva@users.noreply.github.com>
2021-06-16 02:53:28 +0800

    Update main.rs

--- 1~100/Problem_49/src/main.rs
+++ 1~100/Problem_49/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
-
+use std::collections::HashMap;
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
 
     let rst: Vec<Vec<String>> = Vec::new();
+    let map: HashMap<String, Vec<String>> = HashMap::new();
     
+    for s in strs {
+
+    }
     rst
 }
