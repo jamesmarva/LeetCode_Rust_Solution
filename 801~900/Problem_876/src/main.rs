1f38993f86e4c14ed77c307c60aad154b47acd63
jameskk <jamesmarva@users.noreply.github.com>
2021-06-18 00:56:36 +0800

    Update main.rs

--- 801~900/Problem_876/src/main.rs
+++ 801~900/Problem_876/src/main.rs
@@ -4,11 +4,10 @@ fn main() {
 pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let mut slow = &head;
     let mut fast = &head;
-    // let node = fast.unwrap();
+    let test = slow.as_ref().unwrap().next;
     while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
-        slow = &slow.as_ref().unwrap().next;
-        fast = &fast.as_ref().unwrap().next;
         
+        fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
     }
     slow.clone()
 }
