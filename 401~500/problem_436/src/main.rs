db17aeb5477578550aab5cc814fb95b2d0c99d60
jameskk <jamesmarva@users.noreply.github.com>
2021-06-27 14:59:18 +0800

    add 436. 寻找右区间
    https://leetcode-cn.com/problems/find-right-interval/

---
+++ 401~500/problem_436/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn find_right_interval(intervals: Vec<Vec<i32>>) -> Vec<i32> {
+    let mut tmp: Vec<(i32, usize)> = Vec::new();
+    for (i, interval) in intervals.iter().enumerate() {
+        tmp.push((interval[0], i));
+    }
+    tmp.sort_by_key(|&(val, _)| val);
+
+    let mut rst = vec![-1; intervals.len()];
+    for i in 0..intervals.len() {
+        let interval = &intervals[i];
+        let target = interval[1];
+        let mut lo = 0;
+        let mut hi = tmp.len() - 1;
+        while lo <= hi {
+            let mid = lo + (hi - lo) / 2;
+            if tmp[mid].0 == target {
+                rst[i] = tmp[mid].1 as i32;
+                break;
+            } else if tmp[mid].0 < target {
+                lo = mid + 1;
+            } else if tmp[mid].0 > target && mid == 0 {
+                rst[i] = tmp[mid].1 as i32;
+                break;
+            } else if tmp[mid].0 > target && tmp[mid - 1].0 < target {
+                rst[i] = tmp[mid].1 as i32;
+                break;
+            } else {
+                hi = mid - 1;
+            }
+        }
+    }
+    rst
+}
+
+
