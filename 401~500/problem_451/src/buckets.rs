eed160675622843b8485f2633e66cdf61e8352d5
jameskk <jamesmarva@users.noreply.github.com>
2021-07-04 02:48:37 +0800

    Update buckets.rs

--- 401~500/problem_451/src/buckets.rs
+++ 401~500/problem_451/src/buckets.rs
@@ -3,16 +3,16 @@ use std::collections::HashMap;
 pub fn frequency_sort(s: String) -> String {
     let mut rst = String::new();
     let mut count_map: HashMap<char, usize> = HashMap::new();
-    let mut maxFrequency: usize = 0;
+    let mut max_frequency: usize = 0;
     s.chars().for_each(|o| {
         *count_map.entry(o).or_insert(0usize) += 1;
-        maxFrequency = maxFrequency.max(count_map[&o] as usize);
+        max_frequency = max_frequency.max(count_map[&o] as usize);
     });
-    let mut buckets = vec![Vec::new(); maxFrequency + 1];
+    let mut buckets = vec![Vec::new(); max_frequency + 1];
     for (k, v) in count_map {
         buckets[v as usize].push(k);
     }
-    for i in (0..=maxFrequency).rev() {
+    for i in (0..=max_frequency).rev() {
         let tmp_count = &buckets[i];
         for c in tmp_count {
             for _ in 0..i {
