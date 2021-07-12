c57ebdfdc67aa7119f8ad478f402d12058929fc0
wanghanwei <jamesmarva@users.noreply.github.com>
2021-07-12 10:23:37 +0800

    add 275. H 指数 II
    https://leetcode-cn.com/problems/h-index-ii/
    275. H 指数 II

---
+++ 201~300/Problem_275/src/binary_search.rs
@@ -0,0 +1,18 @@
+pub fn h_index(citations: Vec<i32>) -> i32 {
+    let l = citations.len();
+    let mut rt = l - 1;
+    let mut lt = 0;
+    while lt <= rt {
+        let mid = lt + ((rt - lt) >> 1);
+        let tmp = (l - mid) as i32;
+        println!("mid: {}; tmp: {}", mid, tmp);
+        if citations[mid] >= tmp && (mid == 0 || citations[mid - 1] < tmp + 1) {
+            return tmp;
+        } else if citations[mid] < tmp {
+            lt = mid + 1;
+        } else {
+            rt = mid - 1;
+        }
+    }
+    0
+}
