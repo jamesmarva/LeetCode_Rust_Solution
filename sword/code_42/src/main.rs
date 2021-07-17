dae751c94826047eb5a30685d53f07775243db81
jameskk <jamesmarva@users.noreply.github.com>
2021-07-17 12:59:32 +0800

    add 剑指 Offer 42. 连续子数组的最大和
    剑指 Offer 42. 连续子数组的最大和
    https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/

--- sword/code_42/src/main.rs
+++ sword/code_42/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec! [-2,1,-3,4,-1,2,1,-5,4];
+    println!("{}", max_sub_array(v));
 }
 
 pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-    
+    let mut tmp = nums[0];
+    let mut rst = tmp;
+    for i in 1..nums.len() {
+        tmp = nums[i].max(tmp + nums[i]);
+        rst = rst.max(tmp);
+    }
+    rst
 }
 
 
