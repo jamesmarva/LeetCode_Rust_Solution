ce2fa625f72df33a32e21c686853745efb577de0
jameskk <jamesmarva@users.noreply.github.com>
2021-12-02 03:18:37 +0800

    11

--- 301~400/problem_400/src/stage_2nd.rs
+++ 301~400/problem_400/src/stage_2nd.rs
@@ -5,17 +5,12 @@ use std::i32::{MAX as max_i32};
 pub fn find_nth_digit(n: i32) -> i32 {
     let mut digits_num = HashMap::new();
     digits_num.insert(0u32, 0i32);
-    let mut max_digit = 1;
     for i in 1..9 {
         let pre_tmp = digits_num.get(&(i - 1)).unwrap();
         let cur_val = i as i32 * 9 * 10i32.pow(i - 1);
-        if max_i32 - cur_val < *pre_tmp {
-            max_digit = i;
-            break;
-        }
-        println!("{}", cur_val + pre_tmp);
         digits_num.insert(i, cur_val + pre_tmp);
     }
+    let mut max_digit = 8;
     if *digits_num.get(&max_digit).unwrap() != max_i32 {
         max_digit = max_digit + 1;
         digits_num.insert(max_digit, max_i32);
@@ -31,7 +26,44 @@ pub fn find_nth_digit(n: i32) -> i32 {
     let begin = 10i32.pow(th_max_i);
     let add_num = after / (th_max_i as i32 + 1);
     let place_th = after % (th_max_i as i32 + 1);
-    (begin + add_num).to_string().chars().nth(place_th as usize).unwrap().to_digit(10).unwrap();
+    (begin + add_num).to_string().chars().nth(place_th as usize).unwrap().to_digit(10).unwrap() as i32
+}
+
+pub fn find_nth_digit_right(n: i32) -> i32 {
+    let mut arr: Vec<(i32, i32)> = Vec::new();
+    arr.push((9, 0));
+    arr.push((189, 1));
+    arr.push((2889, 2));
+    arr.push((38889, 3));
+    arr.push((488889, 4));
+    arr.push((5888889, 5));
+    arr.push((68888889, 6));
+    arr.push((788888889, 7));
+    arr.push((2147483647, 8));
+    let mut prev = 0;
+    let mut curr = 0;
+    let mut bit_num = 0;
+    for (k, v) in arr.iter() {
+        if n <= *k {
+            curr = *k;
+            bit_num = *v;
+            break;
+        } else {
+            prev = *k;
+        }
+    }
+    // println!("{:?}", (prev, curr, bit_num));
+    let dis = n - (prev + 1);
+    // println!("dis: {}", &dis);
+
+    let num: i32 = dis / (bit_num + 1);
+    println!("num: {}", &num);
+
+    let s = (10i32.pow(bit_num as u32) + num).to_string();
+    println!("s {}", s);
+    let th = dis  % (bit_num + 1);
+    let s: Vec<char> = s.chars().collect();
+    (s[th as usize] as u8 - b'0') as i32
 }
 
 #[cfg(test)]
@@ -42,6 +74,10 @@ mod tests {
     fn t1() {
         // println!("{}", i32::MAX);
         println!("d {}", find_nth_digit(222));
+        println!("d {}", find_nth_digit_right(222));
+
+        // println!("d {}", find_nth_digit(10));
+        // println!("d {}", find_nth_digit(11));
     }
 
 }
